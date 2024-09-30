608d84629a5f0b32608f5175c6aee07f9ff407b7
Aayush Joglekar <aayushjog@gmail.com>
2024-09-30 11:46:11 +0200

    Add negative cycle mode to floyd warshall

--- floyd-warshall-serial.jl
+++ floyd-warshall-serial.jl
@@ -30,54 +30,65 @@ function floyd_warshall!(dp::Matrix{Float64}, next::Matrix{Int64}; negative_cycl
         end
     end
 
-    if negative_cycle_mode
-        if negative_cycle_detected
-            println("Detected a negative cycle!\n")
-        else
-            println("No negative cycles detected!\n")
-        end
-    end
+    negative_cycle_detected
 end
 
 function reconstruct_path(next::Matrix{Int64}, src::Int64, dest::Int64)
-    path = [src]
+    path = [src - 1]
     while src != dest
         src = next[src, dest]
         src == -1 && return nothing
-        push!(path, src)
+        push!(path, src - 1)
     end
     path
 end
 
 function reconstruct_all_paths(next::Matrix{Int64})
+    println("All possible paths:\n")
     for src in 1:N
         for dest in 1:N
             if src != dest
                 path = reconstruct_path(next, src, dest)
-                println("$src -> $dest: ", path !== nothing ? path : "nope")
+                path !== nothing && println("$(src - 1) -> $(dest - 1): ", path)
             end
         end
     end
 end
 
-N::Int64 = 4
+N::Int64 = 7
 
-m = [
-    0.0 Inf Inf 1.0;
-    2.0 0.0 3.0 9.0;
-    Inf Inf 0.0 Inf;
-    Inf Inf 5.0 0.0
-]::Matrix{Float64}
+m::Matrix{Float64} = fill(Inf, N, N)
+for i in 1:N
+    m[i, i] = 0.0
+end
 
-dp = zeros(Float64, N, N)
-next = zeros(Int64, N, N)
+m[1, 2] = 2
+m[2, 4] = 4
+m[4, 3] = -6
+m[3, 2] = 1
+m[1, 3] = 1
+m[4, 5] = 1
+m[5, 7] = 3
+m[6, 7] = 1
+m[5, 6] = 1
+m[4, 6] = 1
+
+@pprint m
+
+dp::Matrix{Float64} = zeros(Float64, N, N)
+next::Matrix{Int64} = zeros(Int64, N, N)
 
 dp .= m
 next .= ifelse.(m .!= Inf, collect(1:N)', -1)
 
 floyd_warshall!(dp, next)
-floyd_warshall!(dp, next; negative_cycle_mode=true)
 
-@pprint m
+negative_cycle_detected = floyd_warshall!(dp, next; negative_cycle_mode=true)
+
+if negative_cycle_detected
+    println("Detected a negative cycle!\n")
+else
+    println("No negative cycles detected!\n")
+end
 
 reconstruct_all_paths(next)
